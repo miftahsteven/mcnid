aa9717db220cc4162a22fa85c7050682361e035d
Miftah Syarief <miftahsyarief@Miftahs-MacBook-Pro-2.local>
2026-03-29 06:46:24 +0700

    google analytics

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,9 +1,25 @@
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
+import Script from 'next/script';
 
 export default function SiteLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-94PR12JYPW"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-94PR12JYPW');
+        `}
+      </Script>
+
       <Header />
       <main className="min-h-screen">{children}</main>
       <Footer />
