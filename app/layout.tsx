f8880e8b334405b369406ece93e968a15008a533
Miftah Syarief <miftahsyarief@Miftahs-MacBook-Pro-2.local>
2026-03-29 06:55:57 +0700

    ubah tempat google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || "https://mcnid.net"),
@@ -42,7 +43,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="id">
-      <body>{children}</body>
+      <body>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-94PR12JYPW"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-94PR12JYPW');
+          `}
+        </Script>
+        {children}
+      </body>
     </html>
   );
 }
